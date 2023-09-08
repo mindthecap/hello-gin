a0b6252f1e7d2dcf44e827c2b1e5081fdb5540cf
Johani Vajakas <mindthecap@users.noreply.github.com>
2023-09-08 13:17:33 +0300

    Added monitor

--- cmd/main.go
+++ cmd/main.go
@@ -10,21 +10,13 @@ import (
 func main() {
 	router := gin.Default()
 
-	metricsRouter := gin.Default()
-
 	metrics := ginmetrics.GetMonitor()
 	metrics.SetMetricPath("/metrics")
-	metrics.Use(metricsRouter)
+	metrics.Use(router)
 
 	router.GET("/api/v1/gin", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello " + os.Getenv("USERNAME") + " from " + os.Getenv("MY_POD_NAME"))
 	})
 
-	// Run the main router on port 8000
-	go func() {
-		router.Run(":8000")
-	}()
-
-	// Run the metrics router on a different port, e.g., 8080
-	metricsRouter.Run(":8080")
+	router.Run(":8000")
 }
